a4211693a9598c64c32818c1d0d9a823b1039702
Chris Williams <cwill833@gmail.com>
2019-07-07 01:26:48 -0500

    first commit

---
+++ src/Components/App/App.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import {SearchBar} from '../SearchBar/SearchBar'
+
+import './App.css';
+
+class App extends Component{
+  render(){
+
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <SearchBar />
+          <div className="App-playlist">
+            {/* <!-- Add a SearchResults component -->} */}
+            <SearchResults />
+            {/* <!-- Add a Playlist component --> */}
+            <Playlist />
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
